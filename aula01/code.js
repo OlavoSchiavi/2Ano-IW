15bcce5b775ec68b63f744f694ddac5fbbb5bcbe
MrStar <mr.sadstar@gmail.com>
2024-02-19 16:55:50 -0300

    Add files via upload

---
+++ aula01/code.js
@@ -0,0 +1,12 @@
+function cnome(){
+    let n = document.getElementById("nomw").value
+    alert("Olá: " + n)
+}
+
+
+function ola(){
+    console.log("Hello world");
+    alert("ze da manga")
+}
+
+ola()
